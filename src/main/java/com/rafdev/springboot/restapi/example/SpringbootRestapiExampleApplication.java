369f21010050e43d0c706591db3506fb6d92da88
Rafiou Sitou <rafiousitou90@gmail.com>
2021-08-21 20:45:36 -0300

    Initial Commit.

---
+++ src/main/java/com/rafdev/springboot/restapi/example/SpringbootRestapiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.rafdev.springboot.restapi.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafdev/springboot/restapi/example/SpringbootRestapiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafdev.springboot.restapi.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
