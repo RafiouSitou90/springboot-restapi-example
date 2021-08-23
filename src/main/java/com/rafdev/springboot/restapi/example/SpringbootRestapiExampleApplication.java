531adcad89141ed89035a2dc16fa80e65e6ff26f
Rafiou Sitou <rafiousitou90@gmail.com>
2021-08-23 13:04:08 -0300

    feat(base-entity): Add BaseEntity.java

--- src/main/java/com/rafdev/springboot/restapi/example/SpringbootRestapiExampleApplication.java
+++ src/main/java/com/rafdev/springboot/restapi/example/SpringbootRestapiExampleApplication.java
@@ -2,8 +2,10 @@ package com.rafdev.springboot.restapi.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootRestapiExampleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rafdev/springboot/restapi/example/entity/Category.java
+++ src/main/java/com/rafdev/springboot/restapi/example/entity/Category.java
@@ -6,7 +6,6 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,7 +17,7 @@ import java.util.Set;
 @Builder
 @ToString
 @Table(name = "tab_categories")
-public class Category
+public class Category extends BaseEntity
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,15 +31,9 @@ public class Category
     private String slug;
 
     @JsonManagedReference
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
     @ToString.Exclude
     private Set<Post> posts = new HashSet<>();
-
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
 
     public void computeSlug() {
         Slugify slugify = new Slugify();
@@ -49,13 +42,11 @@ public class Category
 
     @PrePersist
     public void prePersist() {
-        createdAt = LocalDateTime.now();
         computeSlug();
     }
 
     @PreUpdate
     public void preUpdate() {
-        updatedAt = LocalDateTime.now();
         computeSlug();
     }
 }
